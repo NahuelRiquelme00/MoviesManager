4fc8283df72adf20be7886293360a1f8d4b8e5f7
Macaaa13 <maca.moya@hotmail.com>
2023-02-16 17:16:56 -0300

    Review modificada

--- app/src/main/java/com/example/moviesmanager/views/ui/ReviewFragment.java
+++ app/src/main/java/com/example/moviesmanager/views/ui/ReviewFragment.java
@@ -1,16 +1,13 @@
 package com.example.moviesmanager.views.ui;
 
 import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.RatingBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -23,15 +20,9 @@ import androidx.lifecycle.ViewModelProvider;
 import com.bumptech.glide.Glide;
 import com.example.moviesmanager.R;
 import com.example.moviesmanager.database.ConsultarDB;
-import com.example.moviesmanager.databinding.FragmentDetalleBinding;
 import com.example.moviesmanager.databinding.FragmentReviewBinding;
-import com.example.moviesmanager.models.Favorita;
 import com.example.moviesmanager.models.Pelicula;
 import com.example.moviesmanager.models.Review;
-import com.example.moviesmanager.models.Valoracion;
-import com.example.moviesmanager.models.VerMasTarde;
-import com.example.moviesmanager.models.YaVista;
-import com.example.moviesmanager.viewmodels.DetalleViewModel;
 import com.example.moviesmanager.viewmodels.ReviewViewModel;
 
 public class ReviewFragment extends Fragment {
@@ -42,9 +33,11 @@ public class ReviewFragment extends Fragment {
     private EditText mReview;
     private Button mGuardar;
     private Button mCancelar;
+    private Button mLimpiar;
+    private LayoutInflater layoutInflater;
+    private View cancelarADView, limpiarADView;
     private Integer id_pelicula;
     private ConsultarDB db;
-
     private ReviewViewModel reviewViewModel;
 
 
@@ -61,6 +54,8 @@ public class ReviewFragment extends Fragment {
         mReview = binding.editTextReviewReview;
         mGuardar = binding.buttonReviewGuardar;
         mCancelar = binding.buttonReviewCancelar;
+        mLimpiar = binding.buttonReviewLimpiar;
+        layoutInflater = getActivity().getLayoutInflater();
 
         db = ConsultarDB.getInstance(getContext().getApplicationContext());
 
@@ -74,13 +69,13 @@ public class ReviewFragment extends Fragment {
             @Override
             public void onClick(View view) {
                 if(mReview.getText().toString().isEmpty()){
-                    Toast.makeText(getContext(), "Ingrese una reseña ", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getContext(), "Error: no se puede guardar una review vacía", Toast.LENGTH_SHORT).show();
                 }else{
                     try{
                         Review review = new Review(id_pelicula);
                         review.setReview(mReview.getText().toString());
                         db.daoReseña().insertarReview(review);
-                        Toast.makeText(getContext(), "Review guardada ", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getContext(), "Review guardada", Toast.LENGTH_SHORT).show();
                         getActivity().onBackPressed();
                     }catch (Exception ex){
                         Toast.makeText(getContext(), "Error al guardar review " + ex, Toast.LENGTH_SHORT).show();
@@ -93,23 +88,56 @@ public class ReviewFragment extends Fragment {
         mCancelar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
-                builder.setTitle("Cancelar")
-                        .setMessage("Esta seguro que desea cancelar?")
-                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialogInterface, int i) {
-                                dialogInterface.cancel();
-                            }
-                        })
-                        .setPositiveButton("Si", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialogInterface, int i) {
-                                getActivity().onBackPressed();
-                            }
-                        });
-                AlertDialog dialog = builder.create();
-                dialog.show();
+                AlertDialog.Builder builderADCancelar = new AlertDialog.Builder(getContext());
+                cancelarADView = layoutInflater.inflate(R.layout.alert_dialog_review_cancelar, null);
+                final Button btCancelarNo = (Button) cancelarADView.findViewById(R.id.buttonADReviewCancelarNo);
+                final Button btCancelarSi = (Button) cancelarADView.findViewById(R.id.buttonADReviewCancelarSi);
+                builderADCancelar.setView(cancelarADView);
+                AlertDialog dialogCancelar = builderADCancelar.create();
+                dialogCancelar.show();
+                btCancelarNo.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        dialogCancelar.cancel();
+                    }
+                });
+                btCancelarSi.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        dialogCancelar.dismiss();
+                        getActivity().onBackPressed();
+                    }
+                });
+            }
+        });
+
+        mLimpiar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(mReview.getText().toString().isEmpty()){
+                    Toast.makeText(getContext(), "Error: Ingrese una review primero", Toast.LENGTH_SHORT).show();
+                } else {
+                    AlertDialog.Builder builderADLimpiar = new AlertDialog.Builder(getContext());
+                    limpiarADView = layoutInflater.inflate(R.layout.alert_dialog_review_limpiar, null);
+                    final Button btLimpiarNo = (Button) limpiarADView.findViewById(R.id.buttonADReviewLimpiarNo);
+                    final Button btLimpiarSi = (Button) limpiarADView.findViewById(R.id.buttonADReviewLimpiarSi);
+                    builderADLimpiar.setView(limpiarADView);
+                    AlertDialog dialogLimpiar = builderADLimpiar.create();
+                    dialogLimpiar.show();
+                    btLimpiarNo.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            dialogLimpiar.cancel();
+                        }
+                    });
+                    btLimpiarSi.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            dialogLimpiar.dismiss();
+                            mReview.setText("");
+                        }
+                    });
+                }
             }
         });
 
